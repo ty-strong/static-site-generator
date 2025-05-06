850f0f23bb322a27de1a1e5d84b19a064560b6ba
ty-strong <sk8moola@gmail.com>
2025-05-06 16:48:59 -0700

    Created a function to return the type of a markdown block as well as added new test cases

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,16 +1,32 @@
 from enum import Enum
+import re
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
     HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
-    UNORDERED_LIST = "unordered_list"
-    ORDERED_LIST = "ordered_list"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
 
 def markdown_to_blocks(markdown):
     string_list = markdown.split("\n\n")
     return [string.strip() for string in string_list if string.strip() != ""]
 
-def block_to_block_type(block_text):
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    lines = block.split("\n")
+    if all(line.startswith(">") for line in lines):
+        return BlockType.QUOTE
+    if all(line.startswith("* ") or line.startswith("- ") for line in lines):
+        return BlockType.ULIST
+    if all(line.startswith(f"{i+1}. ") for i, line in enumerate(lines)):
+        return BlockType.OLIST
+    else:
+        return BlockType.PARAGRAPH
+    
+def markdown_to_html_node(markdown):
     pass

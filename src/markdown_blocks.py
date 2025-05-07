355724dae6b420e478a68fee98d67a039e63481d
ty-strong <sk8moola@gmail.com>
2025-05-07 16:21:15 -0700

    Markdown text can now be parsed into HTMLNodes using the markdown_to_html_node function

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,8 @@
 from enum import Enum
 import re
+from htmlnode import ParentNode
+from inline_markdown import text_to_textnodes
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -16,12 +19,12 @@ def markdown_to_blocks(markdown):
 def block_to_block_type(block):
     if re.match(r"^#{1,6} ", block):
         return BlockType.HEADING
-    if block.startswith("```") and block.endswith("```"):
-        return BlockType.CODE
     lines = block.split("\n")
+    if len(lines) > 1 and block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
     if all(line.startswith(">") for line in lines):
         return BlockType.QUOTE
-    if all(line.startswith("* ") or line.startswith("- ") for line in lines):
+    if all(line.startswith("- ") for line in lines):
         return BlockType.ULIST
     if all(line.startswith(f"{i+1}. ") for i, line in enumerate(lines)):
         return BlockType.OLIST
@@ -29,4 +32,83 @@ def block_to_block_type(block):
         return BlockType.PARAGRAPH
     
 def markdown_to_html_node(markdown):
-    pass
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        children.append(block_to_html_node(block))
+    return ParentNode("div", children)
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    match block_type:
+        case BlockType.PARAGRAPH:
+            return paragraph_to_html_node(block)
+        case BlockType.HEADING:
+            return heading_to_html_node(block)
+        case BlockType.CODE:
+            return code_to_html_node(block)
+        case BlockType.QUOTE:
+            return quote_to_html_node(block)
+        case BlockType.ULIST:
+            return ulist_to_html_node(block)
+        case BlockType.OLIST:
+            return olist_to_html_node(block)
+        case _:
+            raise ValueError("invalid block type")
+            
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    children = []
+    for node in text_nodes:
+        children.append(text_node_to_html_node(node))
+    return children
+
+def paragraph_to_html_node(block):
+    paragraph = " ".join(block.split("\n"))
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def heading_to_html_node(block):
+    parts = block.split(maxsplit=1)
+    if parts:
+        num_pound_signs = parts[0].count('#')
+        if num_pound_signs >= len(block):
+            raise ValueError(f"invalid heading level: {num_pound_signs}")
+        text = parts[1]
+        children = text_to_children(text)
+        return ParentNode(f"h{num_pound_signs}", children)
+
+def code_to_html_node(block):
+    text = block[4:-3]
+    text_node = TextNode(text, TextType.TEXT)
+    child = text_node_to_html_node(text_node)
+    code = ParentNode("code", [child])
+    return ParentNode("pre", [code])
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    quote_lines = []
+    for line in lines:
+        quote_lines.append(line.lstrip(">").strip())
+    text = " ".join(quote_lines)
+    children = text_to_children(text)
+    return ParentNode("blockquote", children)
+
+def ulist_to_html_node(block):
+    lines = block.split("\n")
+    list_items = []
+    for line in lines:
+        text = line[2:]
+        children = text_to_children(text)
+        list_items.append(ParentNode("li", children))
+    return ParentNode("ul", list_items)
+
+def olist_to_html_node(block):
+    lines = block.split("\n")
+    list_items = []
+    for line in lines:
+        text = line[3:]
+        children = text_to_children(text)
+        list_items.append(ParentNode("li", children))
+    return ParentNode("ol", list_items)

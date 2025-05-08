a5e050acc27d3b86445cb7cc8c1f763adedba2d4
ty-strong <sk8moola@gmail.com>
2025-05-08 16:04:28 -0700

    Running the program now copies all the contents of static to public

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-    node = TextNode("This is a text node", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    public_path = "public"
+    static_path = "static"
+    # remove and recreate the dest dir if it exits, otherwise just create it
+    if os.path.exists(public_path):
+        shutil.rmtree(public_path)
+        os.mkdir(public_path)
+    else:
+        os.mkdir(public_path)
+    copy_contents(static_path, public_path)
+
+def copy_contents(src, dest):
+    src_contents = os.listdir(src)
+    for item in src_contents:
+        src_path = os.path.join(src, item)
+        dest_path = os.path.join(dest, item)
+        if os.path.isfile(src_path):
+            #copy the file to the same location in the dest path
+            shutil.copy(src_path, dest_path)
+        else:
+            # create the directory at the current path in the dest directory and pass both of these into the recursive function
+            os.mkdir(dest_path)
+            copy_contents(src_path, dest_path)
+    return
 
 if __name__ == "__main__":
     main()
